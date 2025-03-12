7ec8293cab254e258258da1badf920f985de218d
Will Copeland <willcopeland34@gmail.com>
2025-03-12 13:21:06 -0700

    Add
    `getAuthUser`
    and `getUser`

--- db/queries/users.ts
+++ db/queries/users.ts
@@ -1,8 +1,31 @@
 "use server";
 import { createClient } from "@/utils/supabase/server";
 
-export async function getUser() {
+export async function getAuthUser() {
   const client = await createClient();
 
-  return client.auth.getUser();
+  const {
+    data: { user },
+    error,
+  } = await client.auth.getUser();
+
+  if (error) throw error;
+  if (!user) throw new Error("No auth user found");
+
+  return user;
+}
+
+export async function getUser() {
+  const authUser = await getAuthUser();
+
+  const client = await createClient();
+  const { data, error } = await client
+    .from("users")
+    .select()
+    .limit(1)
+    .eq("id", authUser.id);
+
+  if (!data?.length) throw new Error("User profile not found");
+
+  return data[0];
 }
