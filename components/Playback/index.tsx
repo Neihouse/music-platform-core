b88be631bd9cd526499d1869905c08a34078900a
Will Copeland <willcopeland34@gmail.com>
2025-03-14 14:47:59 -0700

    save prgo

---
+++ components/Playback/index.tsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+
+export interface IPlaybackProps {
+  src: string;
+}
+
+const PlayerContext = createContext({
+  src: "",
+});
+
+export function Playback({ src }: IPlaybackProps) {
+  return (
+    <PlayerContext.Provider>
+      <AudioPlayer src={src} />
+    </PlayerContext.Provider>
+  );
+}
