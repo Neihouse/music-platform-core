0d6caa70e8c84a49921ecdc8723dd35ee4f6c680
Will Copeland <willcopeland34@gmail.com>
2025-04-05 13:26:47 -0700

    Rename signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,7 @@
 import { Signup } from "@/components/auth/Signup";
 import { Container, Box } from "@mantine/core";
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <Container size="xs" py="xl">
       <Box maw={400} mx="auto">
