20f619d7700c6d20d0b2db522c0a00ba7bb5b0fe
Will Copeland <willcopeland34@gmail.com>
2025-05-06 20:19:43 -0700

    Update types

--- utils/supabase/global.types.ts
+++ utils/supabase/global.types.ts
@@ -3,3 +3,5 @@ import { Database } from "./database.types";
 type Public = Database["public"]["Tables"];
 export type Track = Public["tracks"]["Row"];
 export type Artist = Public["artists"]["Row"];
+export type Locality = Public["localities"]["Row"];
+export type AdministrativeArea = Public["administrative_area"]["Row"];
