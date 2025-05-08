c0d7c1b404e376c8a5c98558b1086f7e47f326a1
Will Copeland <willcopeland34@gmail.com>
2025-05-07 20:40:17 -0700

    Refactor Playback component to use createClient for fetching track play URL

--- components/playback/index.tsx
+++ components/playback/index.tsx
@@ -3,6 +3,7 @@ import "react-h5-audio-player/lib/styles.css";
 import { useEffect, useState } from "react";
 import PlayerContext from "@/lib/PlayerContext";
 import { getTrackPlayURL } from "@/db/queries/tracks";
+import { createClient } from "@/utils/supabase/client";
 
 export interface IPlaybackProps {
   children: React.ReactNode;
@@ -12,6 +13,7 @@ export function Playback({ children }: IPlaybackProps) {
   const [error, setError] = useState<string | null>(null);
   const [trackId, setTrackId] = useState<string | null>(null);
   const [playUrl, setPlayUrl] = useState<string | undefined>(undefined);
+  const supabase = createClient();
 
   useEffect(() => {
     if (!trackId) {
@@ -19,7 +21,7 @@ export function Playback({ children }: IPlaybackProps) {
     }
     async function handlePlay(trackId: string) {
       try {
-        setPlayUrl(await getTrackPlayURL(trackId));
+        setPlayUrl(await getTrackPlayURL(supabase, trackId));
       } catch (error) {
         console.error("Error fetching play URL:", error);
 
