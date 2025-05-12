2e927882a037ef482f71273926b85e1f173e39ef
Will <willcopeland34@gmail.com>
2025-05-11 19:31:41 -0700

    Update ArtistCreatePage to fetch user and artist data, passing artist to ArtistForm for improved functionality.

--- app/artists/create/page.tsx
+++ app/artists/create/page.tsx
@@ -1,12 +1,19 @@
 import { ArtistForm } from "@/components/onboarding/ArtistForm";
+import { getArtist } from "@/db/queries/artists";
+import { getUser } from "@/db/queries/users";
+import { createClient } from "@/utils/supabase/server";
 import * as React from "react";
 
-export interface IArtistCreatePageProps {}
+export interface IArtistCreatePageProps { }
 
-export default async function ArtistCreatePage({}: IArtistCreatePageProps) {
+export default async function ArtistCreatePage({ }: IArtistCreatePageProps) {
+  const supabase = await createClient();
+  const user = await getUser(supabase);
+  const artist = await getArtist(supabase);
+
   return (
     <div>
-      <ArtistForm />
+      <ArtistForm artist={artist} />
     </div>
   );
 }
