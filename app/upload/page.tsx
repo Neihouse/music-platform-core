13b6a234d09038ff10b625422d511353357a9a07
Will <willcopeland34@gmail.com>
2025-05-21 15:02:35 -0700

    Refactor upload components and update imports for ArtistArtUpload in ArtistForm

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { TrackUpload } from "@/components/track-upload";
+import { TrackUpload } from "@/components/Upload";
 import * as React from "react";
 
-export interface IUploadTrackPageProps {}
+export interface IUploadTrackPageProps { }
 
-export default function UploadTrackPage({}: IUploadTrackPageProps) {
+export default function UploadTrackPage({ }: IUploadTrackPageProps) {
   return (
     <div>
       <TrackUpload />
