02b6a2f0a18155bf46aab146111729035392d321
Will Copeland <willcopeland34@gmail.com>
2025-06-05 11:20:58 -0700

    Implement async login page with user redirection when logged in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,18 @@
 import { Login } from "@/components/auth/Login";
+import { getUser } from "@/db/queries/users";
+import { createClient } from "@/utils/supabase/server";
 import { Container, Box } from "@mantine/core";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  // TODO: Implement in middleware.ts
+  const supabase = await createClient()
+  const user = await getUser(supabase);
+
+  if (user) {
+    redirect("/");
+  }
+
   return (
     <Container size="xs" py="xl">
       <Box maw={400} mx="auto">
