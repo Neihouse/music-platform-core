073a5034866a6e3a1d6beb35d6b8a7196fa0360d
Will Copeland <willcopeland34@gmail.com>
2025-06-28 00:42:24 -0700

    refactor: Remove StyledTitleDemo and related exports for cleaner component structure

--- components/StyledTitle/exports.ts
+++
@@ -1,6 +0,0 @@
-// Main component export
-export { default as StyledTitle } from './index';
-export { default as StyledTitleDemo } from './demo';
-
-// Default export for convenience  
-export { default } from './index';
